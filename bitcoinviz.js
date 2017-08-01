8041aea96cb22b88b7bb1d0e60116f4966f2485e
ananthajanakiraman <30395005+ananthajanakiraman@users.noreply.github.com>
2017-07-31 22:11:57 -0500

    Update bitcoinviz.js

--- bitcoinviz.js
+++ bitcoinviz.js
@@ -287,15 +287,14 @@
 	        .style("font-weight", "bold")
 	        .style("font-family","sans-serif")
 	       
-	   svg.append("line").attr("class", "x--line")
- 	        .style("stroke", "#777")
-		.style("shape-rendering", "crispEdges")
-		.style("stroke-dasharray", "1,1")
-		.style("opacity", 0.8)
-		.attr("y1",y(maximumObj.price))
-		.attr("y2",(y(maximumObj.price)+10))	
-	        .attr("x1",x(maximumObj.date))
-		.attr("x2",(x(maximumObj.date)+10));
+	       	svg.append("text")
+	        .attr("class","text1")
+		.attr("x",x(maximum1))
+		.attr("y",y(maximum1))
+		.text('This is a test text')
+	        .style("font-size","10px")
+	        .style("font-weight", "bold")
+	        .style("font-family","sans-serif")       
 	       
        d3.selectAll('input[name="BTHY"]').on("change", change);
 	       
